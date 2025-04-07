41e5f056e4f7ac7bd7f8012ef90b6075595e0549
paulustimothy <paulustimothy011@gmail.com>
2025-04-07 15:30:17 +0700

    2. Add analytics data fetching and visualization in frontend
    - Enhance AnalyticsTab component to fetch and display user, product, sales, and revenue data.
    - Implement loading state and error handling for data fetching.
    - Introduce responsive line chart for daily sales and revenue visualization.
    - Add AnalyticsCard component for displaying key metrics with animations.

--- backend/routes/analytics.route.js
+++ backend/routes/analytics.route.js
@@ -4,6 +4,6 @@ import { getAnalytics } from "../controllers/analytics.controller.js";
 
 const router = express.Router();
 
-router.get("/", protectRoute, isAdmin ,getAnalytics);
+router.get("/", protectRoute, isAdmin, getAnalytics);
 
 export default router;
